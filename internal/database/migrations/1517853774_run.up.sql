89b7d700063964997d4164e53723ecb0d93d6f24
Christine Dodrill <me@christine.website>
2018-02-05 11:12:20 -0800

    ocd

--- internal/database/migrations/1517853774_run.up.sql
+++ internal/database/migrations/1517853774_run.up.sql
@@ -1,19 +1,19 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE IF NOT EXISTS runs
- ( id UUID PRIMARY KEY DEFAULT uuid_generate_v1mc()
- , created_at TIMESTAMP    NOT NULL DEFAULT NOW()
- , check_ids TEXT[] NOT NULL
- , message TEXT
+ ( id         UUID      PRIMARY KEY DEFAULT uuid_generate_v1mc()
+ , created_at TIMESTAMP NOT NULL DEFAULT NOW()
+ , check_ids  TEXT[]    NOT NULL
+ , message    TEXT      NOT NULL DEFAULT ''
  );
 
 CREATE TABLE IF NOT EXISTS run_info
- ( id SERIAL
- , created_at TIMESTAMP    NOT NULL DEFAULT NOW()
- , edited_at  TIMESTAMP    NOT NULL DEFAULT NOW()
- , check_id TEXT NOT NULL
- , response_time_nanoseconds BIGINT NOT NULL
- , webhook_response_time_nanoseconds BIGINT NOT NULL
+ ( id                                SERIAL
+ , created_at                        TIMESTAMP NOT NULL DEFAULT NOW()
+ , edited_at                         TIMESTAMP NOT NULL DEFAULT NOW()
+ , check_id                          TEXT      NOT NULL
+ , response_time_nanoseconds         BIGINT    NOT NULL
+ , webhook_response_time_nanoseconds BIGINT    NOT NULL
  );
 
 CREATE INDEX IF NOT EXISTS run_info_check_id ON run_info(check_id);
